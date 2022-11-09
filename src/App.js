dd5a45f9554e3547263ee433447337574ac844de
will-oracions <oracions.dev@gmail.com>
2022-11-09 20:36:08 +0100

    save exercice 1.3 of part 1

--- src/App.js
+++ src/App.js
@@ -5,28 +5,27 @@ import Total from './components/Total';
 const App = () => {
   const course = 'Half Stack application develoment';
 
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  };
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    }
+  ];
   return (
     <div>
       <Header course={course} />
 
-      <Content parts={[part1,part2,part3]} />
+      <Content parts={parts} />
       
-      <Total total={part1.exercises+part2.exercises+part3.exercises} />
+      <Total total={parts.reduce((sum, n) => sum+=n.exercises, 0)} />
     </div>
   );
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -2,7 +2,7 @@ import Part from './Part';
 
 const Content = (props) => {
   return (
-    props.parts.map((part) => <Part part={part} />)
+    props.parts.map((part, index) => <Part key={index} part={part} />)
   );
 }
 
