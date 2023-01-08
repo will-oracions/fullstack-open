63f884bb77a55a55a14072fa013c0241b39595dd
will-oracions <oracions.dev@gmail.com>
2023-01-08 22:09:40 +0100

    save footer

--- src/comparoshop-b2b/components/footer.jsx
+++ src/comparoshop-b2b/components/footer.jsx
@@ -6,8 +6,42 @@ const Footer = () => {
     }
 
     return <>
-        <div style={footerStyles} className={`h-[400px]`}>
+        <div style={footerStyles} className={`h-[400px] flex flex-col justify-between`}>
+            <div className={`flex justify-around  p-${styles.defaultPadding}`}>
+                <div className={`w-[300px]`}>
+                    Logo
+                </div>
+                <div className={`px-16 bg-green-500 flex-grow`}>
+                    <h3 className={`font-bold`}>Lien rapides</h3>
+                    <ul className={`py-8 h-10  border border-red-500`}>
+                        <li>Technologies</li>
+                        <li>Partenaires</li>
+                        <li>Temoignages</li>
+                        <li>A propos</li>
+                        <li>Contacts</li>
+                    </ul>
+                </div>
+                <div className={`w-[300px] pl-4`}>
+                    <h3>Retrouvez-nous sur</h3>
+                    <ul className={`flex items-center m-4 ml-0`}>
+                        <li><span className={`py-3 px-5 mr-2 rounded-sm bg-[${styles.colors.orange}]`}>f</span></li>
+                        <li><span className={`py-3 px-5 mr-2 rounded-sm bg-[${styles.colors.orange}]`}>tw</span></li>
+                        <li><span className={`py-3 px-5 mr-2 rounded-sm bg-[${styles.colors.orange}]`}>in</span></li>
+                    </ul>
 
+                    <select>
+                        <option>Français</option>
+                        <option>Anglais</option>
+                        <option>Arabe</option>
+                    </select>
+                </div>
+
+                <hr />
+
+            </div>
+            <div className={`h-16 flex items-center justify-center text-center border-t border-[#0001]`}>
+                <p>2022 - 2023 COMPAROSHOP, Tous droits reservés.</p>
+            </div>
         </div>
     </>;
 }
--- src/comparoshop-b2b/components/tech/TechSection.jsx
+++ src/comparoshop-b2b/components/tech/TechSection.jsx
@@ -1,5 +1,6 @@
 // import techBg from '../../Assets/SVG/Pattern02.svg';
 import * as styles from '../../utils/tailwind.styles';
+import fakeImage from '../../Assets/fakeimage.png';
 
 const TechSection = () => {
     const techSectionStyles = {
@@ -7,8 +8,21 @@ const TechSection = () => {
     }
 
     return <>
-        <div style={techSectionStyles} className="h-[600px]">
+        <div style={techSectionStyles} className={`p-${styles.defaultPadding} h-[600px]`}>
+            <div className={`flex justify-around items-center`}>
+                <div>
+                    <div className={`relative w-[400px] h-[250px] bg-white rounded-3xl`}>
+                        <div className={`absolute h-24 h-24 rounded-full -top-10 -left-10`}>
+                            <img className={`w-full h-full`} src={fakeImage} alt="" />
+                        </div>
 
+                    </div>
+                </div>
+                <div>
+                    <h3>Technologies</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt sint aperiam, pariatur commodi eligendi laborum reprehenderit excepturi quod magnam ipsam expedita ipsa provident et cum, deserunt earum omnis, accusantium dolor?</p>
+                </div>
+            </div>
         </div>
     </>;
 }
