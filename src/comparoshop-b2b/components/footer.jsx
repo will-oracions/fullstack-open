35d862275efe5b5a8de7edb6d411b473b26f8876
will-oracions <oracions.dev@gmail.com>
2023-01-10 10:38:17 +0100

    save

--- src/comparoshop-b2b/components/footer.jsx
+++ src/comparoshop-b2b/components/footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 
     return <>
         <div style={footerStyles} className={`flex flex-col justify-between text-[${styles.colors.black}]`}>
-            <div className={`py-20 max-w-[1000px] w-full mx-auto flex flex-col items-center md:flex-row md:justify-around  p-${styles.defaultPadding}`}>
+            <div className={`py-20 max-w-[1000px] w-full mx-auto flex flex-col items-center md:items-start md:flex-row md:justify-around  p-${styles.defaultPadding}`}>
                 <div className={`w-[300px] hidden md:flex justify-end items-center`}>
                     <div className={`cursor-pointer w-24 h-auto text-end`}>
                         <img className={`w-full h-full`} src={logoMin} alt="logo min" />
@@ -18,13 +18,13 @@ const Footer = () => {
                     <h3 className={`text-center md:text-start font-bold text-lg text-[${styles.colors.black}]`}>Lien rapides</h3>
                     <div className={`flex flex-col md:flex-row items-center md:items-start md:justify-between mt-4`}>
                         <ul className={`text-center md:text-start`}>
-                            <li className={`cursor-pointer hover:underline`}>Technologies</li>
-                            <li className={`cursor-pointer hover:underline`}>Partenaires</li>
+                            <li className={`pb-2 cursor-pointer hover:underline`}>Technologies</li>
+                            <li className={`pb-2 cursor-pointer hover:underline`}>Partenaires</li>
                         </ul>
 
                         <ul className={`text-center md:text-start`}>
-                            <li className={`cursor-pointer hover:underline`}>Temoignages</li>
-                            <li className={`cursor-pointer hover:underline`}>A propos</li>
+                            <li className={`pb-2 cursor-pointer hover:underline`}>Temoignages</li>
+                            <li className={`pb-2 cursor-pointer hover:underline`}>A propos</li>
                         </ul>
                         
                         <ul className={`text-center md:text-start`}>
@@ -35,9 +35,9 @@ const Footer = () => {
                 <div className={`w-full max-w-[300px] md:pl-4 text-center md:text-start`}>
                     <h3  className={`text-center md:text-start font-bold text-md text-[${styles.colors.black}]`}>Retrouvez-nous sur</h3>
                     <ul className={`flex justify-center md:justify-start items-center m-1 mt-4 md:m-4 md:ml-0`}>
-                        <li><span className={`block cursor-pointer py-1 px-3 md:py-3 md:px-5 mr-2 rounded-sm bg-[${styles.colors.orange}] hover:underline`}>f</span></li>
-                        <li><span className={`block cursor-pointer py-1 px-3 md:py-3 md:px-5 mr-2 rounded-sm bg-[${styles.colors.orange}] hover:underline`}>tw</span></li>
-                        <li><span className={`block cursor-pointer py-1 px-3 md:py-3 md:px-5 mr-2 rounded-sm bg-[${styles.colors.orange}] hover:underline`}>in</span></li>
+                        <li><span className={`block cursor-pointer py-1 px-3 md:py-3 md:px-5 mr-2 rounded-sm bg-[${styles.colors.orange}]`}>f</span></li>
+                        <li><span className={`block cursor-pointer py-1 px-3 md:py-3 md:px-5 mr-2 rounded-sm bg-[${styles.colors.orange}]`}>tw</span></li>
+                        <li><span className={`block cursor-pointer py-1 px-3 md:py-3 md:px-5 mr-2 rounded-sm bg-[${styles.colors.orange}]`}>in</span></li>
                     </ul>
 
                     <select className={`px-10 py-2 rounded-md my-2 bg-transparent border border-[${styles.colors.orange}] outline-none`}>
