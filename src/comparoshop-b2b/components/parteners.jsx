12462b7e5d5483e08f49a47815cd7bfd2003ff42
will-oracions <oracions.dev@gmail.com>
2023-01-10 18:02:52 +0100

    save

--- src/comparoshop-b2b/components/parteners.jsx
+++ src/comparoshop-b2b/components/parteners.jsx
@@ -1,7 +1,46 @@
+import logo1 from '../Assets/Logo/image24.png';
+import logo2 from '../Assets/Logo/image25.png';
+import logo3 from '../Assets/Logo/image26.png';
+import logo4 from '../Assets/Logo/image27.png';
+import logo5 from '../Assets/Logo/image28.png';
+import logo6 from '../Assets/Logo/image29.png';
+import logo7 from '../Assets/Logo/image30.png';
+import * as styles from '../utils/tailwind.styles';
+
+
 const PartenersSection = () => {
     return <>
         <div className="h-[350px]">
+            <div className={`${styles.container}`}>
+                <h3>Il nous font confiance</h3>
 
+                <div className={`bg-green-400`}>
+
+                    <div className={`flex`}>
+                        <div className={`h-16 mr-16`}>
+                            <img className={`h-full w-auto`} src={logo1} alt="" />
+                        </div>
+                        <div className={`h-16 mr-16`}>
+                            <img className={`h-full w-auto`} src={logo2} alt="" />
+                        </div>
+                        <div className={`h-16 mr-16`}>
+                            <img className={`h-full w-auto`} src={logo3} alt="" />
+                        </div>
+                        <div className={`h-16 mr-16`}>
+                            <img className={`h-full w-auto`} src={logo4} alt="" />
+                        </div>
+                        <div className={`h-16 mr-16`}>
+                            <img className={`h-full w-auto`} src={logo5} alt="" />
+                        </div>
+                        <div className={`h-16 mr-16`}>
+                            <img className={`h-full w-auto`} src={logo6} alt="" />
+                        </div>
+                        <div className={`h-16 mr-16`}>
+                            <img className={`h-full w-auto`} src={logo7} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </>;
 }
