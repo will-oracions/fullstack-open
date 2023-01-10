40169895e463e4443996f28ed3dd79fc66ec4545
will-oracions <oracions.dev@gmail.com>
2023-01-10 11:01:07 +0100

    design contact

--- src/comparoshop-b2b/components/contactUsSection.jsx
+++ src/comparoshop-b2b/components/contactUsSection.jsx
@@ -1,4 +1,5 @@
 import * as styles from '../utils/tailwind.styles';
+import contactSecImage from '../Assets/Img/no-messages.png';
 
 const contactUsStyles = {
     formCard: {
@@ -20,8 +21,28 @@ const ContactUsSection = () => {
                 </div>
             </div>
 
-            <div style={contactUsStyles.infoCard} className={`h-[300px]`}>
-
+            <div style={contactUsStyles.infoCard} className={`h-[300px] flex items-center mx-6`}>
+                <div className={`flex justify-around items-center text-[${styles.colors.white}] w-full max-w-[900px] bg-[${styles.colors.black}] h-48 rounded-xl`}>
+                    <div className={`hidden md:block h-full py-4`}>
+                        <img className={`w-auto h-full`} src={contactSecImage} alt="" />
+                    </div>
+                    <div>
+                        <ul>
+                            <li className={`pb-4`}>
+                                <span>Icon </span>
+                                <span>00237 6 XX XX XX</span>
+                            </li>
+                            <li className={`pb-4`}>
+                                <span>Icon </span>
+                                <span>Douala, Cameroun</span>
+                            </li>
+                            <li>
+                                <span>Icon </span>
+                                <span>Contact@comparoshop.com</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </>;
